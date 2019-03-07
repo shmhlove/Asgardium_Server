39dafd0dcbfbb90139a29bc1e56eec1addd7c00e
SangHo Lee <shmhlove@naver.com>
2019-03-08 00:20:58 +0900

    [마이닝] 서버 호스트 변경

--- src/modules/Config.js
+++ src/modules/Config.js
@@ -1,7 +1,7 @@
 module.exports = 
 {
-    //server_host : "13.124.43.70",
-    server_host : "localhost",
+    server_host : "13.124.43.70",
+    //server_host : "localhost",
     server_port : 3001,
     server_port_for_https : 3002,
     
